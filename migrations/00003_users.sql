27a2085596f1bafe806ec0662b643d2c3b7b7caa
Damir Baktygaliyev <damir050602@gmail.com>
2025-05-21 17:44:02 +0500

    add keycloak user sub to course database

--- migrations/00003_users.sql
+++ migrations/00003_users.sql
@@ -11,11 +11,13 @@ CREATE TABLE users
     roles      varchar(255) NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
-    deleted_at timestamp with time zone
+    deleted_at timestamp with time zone,
+    sub    varchar(36)           not null
 );
 
 -- Create indexes
 CREATE UNIQUE INDEX idx_users_username ON users (username);
+CREATE UNIQUE INDEX idx_users_sub ON users (sub);
 CREATE UNIQUE INDEX idx_users_email ON users (email);
 CREATE INDEX idx_users_deleted_at ON users (deleted_at);
 
